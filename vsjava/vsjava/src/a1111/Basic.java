1fe22e4a0a01d9ef6b2d9eb1449d383fe2553cd1
ansangcuru <ansangcuru@gmail.com>
2024-11-11 17:01:29 +0900

    11

---
+++ vsjava/vsjava/src/a1111/Basic.java
@@ -0,0 +1,11 @@
+package a1111;
+
+public class Basic {
+  public static void main(String[] args) {
+    System.out.println("자바 환영");
+    int x =1;
+    int y = 2;
+    int result=x+y;
+    System.out.println(result);
+  }
+}
